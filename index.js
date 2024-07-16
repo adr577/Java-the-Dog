1b8cafa250ba5ea72aa2ba4a8a219414fcdd9f7d
adr577 <adrian003hernandez@gmail.com>
2024-07-16 11:49:13 -0700

    more adjustments(added javascript)

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var gifEl = document.querySelector('.java-vid-el');
+
+    function changeBackground() {
+        gifEl.style.backgroundImage = "url('./images/gif-after.gif')";
+    }
+
+    gifEl.addEventListener('click', changeBackground);
+    gifEl.addEventListener('touchend', changeBackground);
+});
 
